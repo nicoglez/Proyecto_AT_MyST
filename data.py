7455aa2f2027777838aa85df259428ab25dc39d7
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-11-22 10:33:53 -0600

    train-test

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ currency="EURUSD"
 data = get_MT5_price(currency, "01-01-2019", "02-02-2021")
 
 # Separar entre data de entranamiento y prueba
-test_data = data[data["time"] <= "01-01-2020"]
-train_data = data[data["time"] > "01-01-2020"]
+train_data = data[data["time"] <= "01-01-2020"]
+test_data = data[data["time"] > "01-01-2020"]
 
 test_dec = fn.est_tec(test_data,fn.bollinger(test_data)[0],fn.stochastic(test_data)[0])
 train_dec = fn.est_tec(train_data,fn.bollinger(train_data)[0],fn.stochastic(train_data)[0])
