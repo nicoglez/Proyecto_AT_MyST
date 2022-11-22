bdfe5ada8effc59c9036d0d92b6d89b72e5ee225
Frida Hernandez <if720476@iteso.mx>
2022-11-22 09:24:10 -0600

    Funcin grafica de capital

--- visualizations.py
+++ visualizations.py
@@ -27,3 +27,12 @@ def convergence_chart(data: dict, target_y: str):
     fig.update_yaxes(title="Value")
     fig.update_traces(line_color="black")
     return fig.show()
+
+# Funcion para plottear grafica de evolucion de capital
+def capital_chart(evolucion: pd.DataFrame, train_or_test: str):
+    fig = px.line(evolucion, x="time", y="evolucion_capital", title=f"Grafica de Evolucion Capital {train_or_test}",
+              height=450, width=1000)
+    fig.update_xaxes(title="Date")
+    fig.update_yaxes(title="Capital")
+    fig.update_traces(line_color="black")
+    return fig.show()
